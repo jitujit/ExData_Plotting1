a9cdb651d84eb523455a68a5793adfa0b4c415d9
jitujit <55954320+jitujit@users.noreply.github.com>
2019-12-07 15:28:03 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,13 @@
+#Read Data
+hoc <- read.table(file = './data/household_power_consumption.txt',sep = ';', skip = 66637,nrows = 2880,header = FALSE, na.strings = "?")
+#hoc <- read.table(file = './data/household_power_consumption.txt',sep = ';',header = FALSE, na.strings = "?")
+header <- read.table(file = './data/household_power_consumption.txt',sep = ';', header = FALSE,nrows = 1, stringsAsFactors = FALSE)
+colnames(hoc) <- unlist(header)
+hoc$DateTime <- strptime(paste(hoc$Date, hoc$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+
+#Generate Plot
+plot(x = hoc$DateTime, y = hoc$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)", cex.lab=.7)
+
+#Save File
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
